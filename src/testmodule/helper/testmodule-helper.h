23849d87f10c4a84a1723a1d6577d8d43450bfa8
Stewart Grant <ssgrant@b09-26.sysnet.ucsd.edu>
2019-02-15 08:14:16 -0800

    added libraries for ns32.9

---
+++ src/testmodule/helper/testmodule-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef TESTMODULE_HELPER_H
+#define TESTMODULE_HELPER_H
+
+#include "ns3/testmodule.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* TESTMODULE_HELPER_H */
+
---
+++ src/testmodule/model/testmodule.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef TESTMODULE_H
+#define TESTMODULE_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* TESTMODULE_H */
+
